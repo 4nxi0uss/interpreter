266a1b850da3f6841175a5adc9fd30ed78b551fe
Artur <artur200212@interia.pl>
2024-05-22 18:59:03 +0200

    feat: creating lexer, token and repl

---
+++ src/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lexerNew := lexer.New(line)
+
+		for tok := lexerNew.NextToken(); tok.Type != token.EOF; tok = lexerNew.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
